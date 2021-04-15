a3e2d8b8ae32c30c2b455006a281a09f2188214e
NguyenVanNinh <ninh14111998@gmail.com>
2021-04-15 21:24:00 +0700

    buoi6

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Models\Posts;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,6 +16,39 @@ use App\Http\Controllers\HomeController;
 */
 
 Route::get('/', function () {
+
+
+    //them
+    $post = new Posts();
+    $post->title = 'huy';
+    $post->slug = 'huy';
+    $post->content = 'huy';
+    $post ->save();
+    dd($post);
+
+    //lay ra
+    // lay ra het
+    $post = Posts::query()->get();
+    //lay theo id
+    $user = DB::table('post')->whereIn('id', [2])->get();
+    dd($post);
+
+
+    //sua
+    $post = Posts::query()->find(1);
+    $post->title = 'hung';
+    $post->save();
+    dd($post);
+
+    //xoa
+    //xoa theo id
+    Posts::destroy([1]);
+    //xoa ko theo id
+    $post = Posts::query()->where('slug', 'ninh')->delete();
+    $post->save();
+
+
+
     return view('welcome');
 });
 Route::get('/home',[HomeController::class, 'index']);
